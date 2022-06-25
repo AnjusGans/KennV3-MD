3095c0a2e6d81883532bd2186c4513a51237deae
AnjusGans <suwardikagusti300@gmail.com>
2022-06-25 09:49:48 +0800

    KennV3-MD

---
+++ settings.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+global.owner = ['6283834558105']
+global.premium = ['6283834558105']
+global.packname = ''
+global.author = 'Wa :62882016646288\nYT : WANGAMING\nIG : wz_store12\nGH : WibsBotz\nTT : wz_store12'
+global.namabotnya = 'Wz Bot'
+global.namaownernya = 'Wz Store'
+global.sessionName = 'qrmd'
+
+
+global.dapkey = 'Kirbotz123'
+global.lolkey = 'Atakbotz'
+global.kirkey = 'KirBotz'
+
+
+global.wame = 'https://wa.me/62882016646288'
+global.gckennbot = 'https://chat.whatsapp.com/L1xzgFdSo3JFu8yXeTtrbM'
+global.ownerNumber = ["62882016646288@s.whatsapp.net"]
+global.email = 'gamingwan92@gmail.com'
+global.yt = 'https://youtube.com/channel/UCsQHOaoM4b1ta7bZVLvc_Pg'
+global.webkenn = 'https://youtube.com/channel/UCsQHOaoM4b1ta7bZVLvc_Pg'
+global.githubkenn = 'Belum ada'
+global.region = 'Indonesia'
+global.footertext = 'KennBot - MD || Created By Timm'
+
+
+global.prefa = ['!','.','#','-','•']
+global.mess = {
+    success: 'Success 💯',
+    admin: 'Fitur Khusus Admin Group!!!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!!!',
+    owner: 'Fitur Khusus Owner Bot!!!',
+    group: 'Fitur Digunakan Hanya Untuk Group!!!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!!!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot!!!',
+    error: 'Error Kak Silahkan Chat Owner',
+    wait: '❗Waittt...',
+    noPetualang: 'Itssss Kamu Belum Jadi Member Petualang, Silahkan Ketik .joinrpg <namamu>',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
+    wrongFormat: 'Format salah, coba liat lagi di menu',
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 30
+}
+
+
+global.thumb = fs.readFileSync('./Image/kirbotz.jpg')
+global.thumbrpg = fs.readFileSync('./Image/kirbotzrpg.jpg')
+global.imgjoinrpg = fs.readFileSync('./Image/joinrpg.jpg')
+global.imgmining = fs.readFileSync('./Image/mining.jpg')
+global.sticknya = fs.readFileSync('./Kir/JANGANDIUBAH/fuck.webp')
+
+global.imgdevil = fs.readFileSync('./Image/img/devil.jpg')
+global.imgslime = fs.readFileSync('./Image/img/slime.jpg')
+global.imgdemon = fs.readFileSync('./Image/img/demon.jpg')
+global.imggoblin = fs.readFileSync('./Image/img/goblin.jpg')
+global.imgdemonking = fs.readFileSync('./Image/img/demonking.jpg')
+global.imgbehemoth = fs.readFileSync('./Image/img/behemoth.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
